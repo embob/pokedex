bab3b8521f378fb2c4ff9051a35d261752a5ee10
Emma Karayiannis <emma.karayiannis@gmail.com>
2021-09-20 15:27:43 +0100

    Better formatting of Names

--- src/components/PreviewCard/PreviewCard.jsx
+++ src/components/PreviewCard/PreviewCard.jsx
@@ -14,6 +14,7 @@ export default function PreviewCard({ id, name, types }) {
     }
   }, {});
 
+  const formattedName = name.split('-').map(word => capitalise(word)).join(' ');
   const { isShowing, toggle } = useModal();
   const typeIcons = types.map((type, index) => <img key={index} className={`preview-card__icon preview-card__icon--${type}`} src={`images/icons/${type}.svg`} alt={type} width={40} height={40} />);
 
@@ -21,7 +22,7 @@ export default function PreviewCard({ id, name, types }) {
     <>
       <div className="preview-card preview-card--gradient" style={colors} onClick={toggle}>
         <img loading="lazy" src={`images/${id}.png`} alt={name} width={175} height={175} />
-        <h2 className="preview-card__name">{capitalise(name)} <span className="id">{`#${id.toString().padStart(3, "0")}`}</span></h2>
+        <h2 className="preview-card__name">{formattedName} <span className="id">{`#${id.toString().padStart(3, "0")}`}</span></h2>
         <div className="types">{typeIcons}</div>
       </div>
       <Modal
